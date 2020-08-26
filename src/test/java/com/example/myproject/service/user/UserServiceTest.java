97e9ceb1c2d3a04a09b3201e2260d87d1a4c535f
ktj1997 <rlaxowns7916@gmail.com>
2020-08-26 21:21:54 +0900

    UserTest

--- src/test/java/com/example/myproject/service/user/UserServiceTest.java
+++ src/test/java/com/example/myproject/service/user/UserServiceTest.java
@@ -1,12 +1,38 @@
 package com.example.myproject.service.user;
 
+import com.example.myproject.model.dto.request.user.SignUpRequestDto;
+import com.example.myproject.model.dto.resonse.user.UserInfoDto;
+import com.example.myproject.service.auth.AuthService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.transaction.Transactional;
 
+@SpringBootTest
+@Transactional
 class UserServiceTest {
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    UserService userService;
+
+    @BeforeEach
+    void UserTestBeforeEach() {
+        SignUpRequestDto signUpRequestDto = new SignUpRequestDto("Test1234", "password123!", "test@gmail.com");
+        authService.signUp(signUpRequestDto);
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("Test1234", "password123!"));
+    }
 
     @Test
     void getUserInfo() {
+        UserInfoDto userInfoDto = Assertions.assertDoesNotThrow(() -> userService.getUserInfo());
+        Assertions.assertNotNull(userInfoDto);
     }
 }
